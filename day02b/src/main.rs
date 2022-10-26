d960bb302a05954d5c69b7f963bb83c52a1247ef
Maximilian Konter <maximilian.konter@protonmail.com>
2022-10-26 10:27:49 +0200

    Day02b

--- day02b/src/main.rs
+++ day02b/src/main.rs
@@ -8,14 +8,19 @@ lazy_static! {
     static ref RE: Regex = Regex::new(r"(up|down|forward) (\d+)$").unwrap();
 }
 
-fn drive(horizontal: &mut i32, vertical: &mut i32, input: &str) {
+fn forward(horizontal: &mut i32, vertical: &mut i32, aim: i32, amount: i32) {
+    *horizontal += amount;
+    *vertical += aim * amount;
+}
+
+fn drive(horizontal: &mut i32, vertical: &mut i32, aim: &mut i32, input: &str) {
     let captures = RE.captures(input).unwrap();
 
     let digits = captures.get(2).map_or(0, |m| m.as_str().parse::<i32>().unwrap());
     match captures.get(1).map_or("", |m| m.as_str()) {
-        "up" => *vertical -= digits,
-        "down" => *vertical += digits,
-        "forward" => *horizontal += digits,
+        "up" => *aim -= digits,
+        "down" => *aim += digits,
+        "forward" => forward(horizontal, vertical, *aim, digits),
         &_ => panic!("Failed to match")
     };
 }
@@ -26,8 +31,9 @@ fn main() {
 
     let mut horizontal: i32 = 0;
     let mut vertical: i32 = 0;
+    let mut aim: i32 = 0;
     for line in contents.lines() {
-        drive(&mut horizontal, &mut vertical, line)
+        drive(&mut horizontal, &mut vertical, &mut aim, line)
     }
 
     let result = horizontal * vertical;
